622febeb98904f0beb1cd2e36a40da4649fd7999
braveness23 <braveness23@gmail.com>
2024-11-30 20:10:31 -0500

    hometemp.db

--- src/openweathermap/weatherdata.py
+++ src/openweathermap/weatherdata.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-conn = sqlite3.connect('sensors.db')
+conn = sqlite3.connect('../../database/hometemp.db')
 cursor = conn.cursor()
 cursor.execute('SELECT * FROM weather_data ORDER BY timestamp DESC LIMIT 1')
 row = cursor.fetchone()
